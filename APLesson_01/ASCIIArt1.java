603519208bf622670f79d8a63ff1f64f7da3773f
jwang9930 <Github98>
2016-09-02 13:39:00 -0700

    Added Files

---
+++ APLesson_01/ASCIIArt1.java
@@ -0,0 +1,13 @@
+public class ASCIIArt1
+{
+		public static void main(String[]args)
+		{
+			System.out.println("                       ----	           ------");
+			System.out.println("                     /       \\~~~~/         \\");
+			System.out.println("      ---------(	      . .             )");
+			System.out.println("    /                \\__ _              ___/");
+			System.out.println("  /|                          ( \\        | (");
+			System.out.println("^   \\        /____\\       /\\     |	");
+			System.out.println("       |__|           |__|     ---");
+		}
+}
---
+++ APLesson_01/ASCIIArt2.java
@@ -0,0 +1,10 @@
+public class ASCIIArt2
+{
+		public static void main(String[]args)
+		{
+			System.out.println("    @. .@");
+			System.out.println("    (------)");
+			System.out.println("  (  >__<  )");
+			System.out.println("^^  ~~  ^^");
+		}
+}
