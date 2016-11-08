7455fbf3a9abcf3a98118f07f999445735171b24
jwang9930 <johnny9986@gmail.com>
2016-11-08 14:25:55 -0800

    Added Lesson_08 Labs

---
+++ APLesson_08/Center.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Center
+{
+	public static void main(String[]args)
+	{
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Enter your first word: ");
+		String word1 = kb.next();
+		System.out.println("Enter your second word: ");
+		String word2 = kb.next();
+		System.out.println("Enter your third word: ");
+		String word3 = kb.next();
+		System.out.println(makeCenter(word1));
+		System.out.println(makeCenter(word2));
+		System.out.println(makeCenter(word3));
+	}
+	
+	public static String makeCenter(String word)
+	{
+		if(word.length() >= 20)
+			return word;
+		else
+			return makeCenter(" " + word + " ");
+	}
+}
---
+++ APLesson_08/Underscores.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Underscores
+{
+	public static void main(String[]args)
+	{
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Please enter a sentence: ");
+		String sentence = kb.nextLine();
+		System.out.println(replace(sentence));
+	}
+	
+	public static String replace(String sentence)
+	{
+		int num = sentence.indexOf(" "); 
+		if (num < 0)
+			return sentence;
+		else
+		{
+			return replace(sentence.substring(0, num) + "_" + sentence.substring(num+1));
+		}
+	}
+}
