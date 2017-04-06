e08700e8374ce37192408ab540be2d263f8edc76
jwang9930 <johnny9986@gmail.com>
2017-04-06 13:59:14 -0700

    update magpie

--- APLesson_15/Magpie/Magpie2.java
+++ APLesson_15/Magpie/Magpie2.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 public class Magpie2
 {
 	public String getGreeting()
@@ -10,7 +8,7 @@ public class Magpie2
 	public String getResponse(String statement)
 	{
 		String response = "";
-		if(statement.length == 0)
+		if(statement.equals(""))
 		{
 			System.out.println("Say something, please.");
 		}
@@ -19,21 +17,21 @@ public class Magpie2
 		{
 			response = "Why so negative?";
 		}
-		else if (statement.indexOf("mother") >= 0
-				|| statement.indexOf("father") >= 0
-				|| statement.indexOf("sister") >= 0
-				|| statement.indexOf("brother") >= 0)
+		else if (findKeyword(statement, "mother", 0) >= 0
+				|| findKeyword(statement, "father", 0) >= 0
+				|| findKeyword(statement, "sister", 0) >= 0
+				|| findKeyword(statement, "brother", 0) >= 0)
 		{
 			response = "Tell me more about your family.";
 		}
-		else if (statement.indexOf("cat") >= 0
-				|| statement.indexOf("dog") >= 0
-				|| statement.indexOf("fish") >= 0
-				|| statement.indexOf("turtle") >= 0)
+		else if (findKeyword(statement, "cat", 0) >= 0
+				|| findKeyword(statement, "dog", 0) >= 0
+				|| findKeyword(statement, "fish", 0) >= 0
+				|| findKeyword(statement, "turtle", 0) >= 0)
 		{
 			response = "Tell me more about your pet.";
 		}
-		else if(statement.indexof("Robinette") >= 0)
+		else if(findKeyword(statement, "robinette", 0) >= 0)
 		{
 			response = "He sounds like a pretty dank teacher.";
 		}
@@ -44,54 +42,45 @@ public class Magpie2
 		return response;
 	}
 
-	/** Ex_02: The findKeyword() Method...
-	 * ========================================================= */
 	private int findKeyword(String statement, String goal, int startPos)
 	{
-		/* New String variable phrase = a more searchable version of statement.
-		 	-Use a combination of trim() and toLowerCase() modify statement.
+		String phrase = (statement.trim()).toLowerCase();
+		goal = goal.toLowerCase();
 
-		   New int variable psn = the location of goal in phrase after
-		   startPos
+		int psn = phrase.indexOf(goal, startPos);
 
-			-->Refinement: Make sure we find goal by itself, and not part
-			of another word ("no" vs no in "know"). if you find an occurrence
-			of goal, make sure before and after aren't letters.
+		while (psn >= 0)
+		{
 
-			As long as psn >= 0...
-				Check if psn > 0 - there is no need to check for before at the
-				beginning of the word
-					set before = the slot in phrase before psn */
+			String before = " ", after = " ";
+			if (psn > 0)
+			{
+				before = phrase.substring(psn - 1, psn);
+			}
+			if (phrase.length() > psn + goal.length() + 1)
+			{
+				after = phrase.substring(psn + goal.length(),
+										psn + goal.length() + 1);
+			}
 
-				//====>code here
-
-				/*check if you can fit goal into the rest of phrase - no need to
-				proceed otherwise
-					set after = the slot in phrase after psn + length of goal */
-
-				//=====> code here
-
-				/* if before and after are not letters (compare before to "a"
-					and after to "z")
-						--return psn
-
-				Otherwise, search for goal in phrase from psn + 1 forward */
+			if (((before.compareTo("a") < 0) || (before.compareTo("z") > 0))
+					&& ((after.compareTo("a") < 0) || (after.compareTo("z") > 0)))
+			{
+				return psn;
+			}
+			else
+				psn = phrase.indexOf(goal, psn + 1);
+		}
 
 		return -1;
 
 	}
 
-	/** Override - this method is used if there are only 2 parameters...*/
 	private int findKeyword(String statement, String goal)
 	{
-		//set startPos to 0 if not specified
 		return findKeyword(statement, goal, 0);
 	}
 
-	/** getRandomResponse() method
-	 * =============================================================*/
-	/** Pick a default response to use if nothing else fits.
-	 * 	@return a non-committal string*/
 	private String getRandomResponse()
 	{
 		final int NUMBER_OF_RESPONSES = 4;
