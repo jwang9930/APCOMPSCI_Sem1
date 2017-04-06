c86d484f4cfebf8ddd982676ec49df9ad74d20b6
jwang9930 <johnny9986@gmail.com>
2017-04-06 13:06:21 -0700

    Updated

--- APLesson_13/Toy Store/ToyStoreRunner.java
+++ APLesson_13/Toy Store/ToyStoreRunner.java
@@ -1,13 +1,10 @@
-import java.util.Scanner;
-
 public class ToyStoreRunner
 {
 	public static void main(String[]args)
 	{
-		Scanner kb = new Scanner(System.in);
-		System.out.println("Input toy list:");
-		String toys = kb.nextLine();
-		ToyStore toystore = new ToyStore(toys);
+		ToyStore toystore = new ToyStore("Hotwheel, Car, G.I.Joe, AF, PennyRacer, Car, Matchbox, Car, Star Wars, AF, Pullback, Car, Star Wars, AF");
 		System.out.println(toystore);
+		System.out.println(toystore.getMostFrequentToy());
+		System.out.println(toystore.getMostFrequentType());
 	}
 }
